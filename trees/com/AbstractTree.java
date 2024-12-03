c7796020dbc3dcf09c4c499d243e44ce28e48d34
Alperen Ulukaya <alperenulukaya07@gmail.com>
2024-12-04 01:18:50 +0300

    iterable preorder postorder BFS in abstractTree class

--- trees/com/AbstractTree.java
+++ trees/com/AbstractTree.java
@@ -1,6 +1,11 @@
 package trees.com;
 
 import lists.com.Position;
+import queues.com.LinkedQueue;
+import queues.com.Queue;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class AbstractTree<E> implements Tree<E> {
     public boolean isInternal(Position<E> p) {
@@ -42,5 +47,55 @@ public abstract class AbstractTree<E> implements Tree<E> {
             height = Math.max(height, 1 + heightBetter(l));
         }
         return height;
+    }
+
+    // for preorder
+    public Iterable<Position<E>> positions() {
+        return preorder();
+    }
+
+    public Iterable<Position<E>> preorder() {
+        List<Position<E>> snapshot = new ArrayList<>();
+        if (!isEmpty())
+            preorderSubtree(root(), snapshot);
+        return snapshot;
+    }
+
+    private void preorderSubtree(Position<E> p, List<Position<E>> snapshot) {
+        snapshot.add(p);
+        for (Position<E> c : children(p)) {
+            preorderSubtree(c, snapshot);
+        }
+    }
+
+    // for postorder
+    public Iterable<Position<E>> postorder() {
+        List<Position<E>> snapshot = new ArrayList<>();
+        if (!isEmpty())
+            postorderSubtree(root(), snapshot);
+        return snapshot;
+    }
+
+    private void postorderSubtree(Position<E> p, List<Position<E>> snapshot) {
+        for (Position<E> c : children(p)) {
+            postorderSubtree(c, snapshot);
+        }
+        snapshot.add(p);
+    }
+
+    // breadthfirst search
+    public Iterable<Position<E>> breadthfirst() {
+        List<Position<E>> snapshot = new ArrayList<>();
+        if (!isEmpty()) {
+            Queue<Position<E>> fringe = new LinkedQueue<>();
+            fringe.enqueue(root());
+            while (!fringe.isEmpty()) {
+                Position<E> p = fringe.dequeue();
+                snapshot.add(p);
+                for (Position<E> c : children(p))
+                    fringe.enqueue(c);
+            }
+        }
+        return snapshot;
     }
 }
